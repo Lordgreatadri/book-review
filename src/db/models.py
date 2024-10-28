021c9a2d6f7ac65e828b4482bc516ac8220a687c
Lordgreat <rexmerlo@gmail.com>
2024-10-28 14:30:14 +0000

    review migrateion added

--- src/db/models.py
+++ src/db/models.py
@@ -32,6 +32,7 @@ class User(SQLModel, table=True):
     created_at:datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now()))
     updated_at:datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now()))
     books: List["Book"] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy":"selectin"})
+    reviews: List["Review"] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy":"selectin"})
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -68,8 +69,10 @@ class Book(SQLModel, table = True):
     created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now()))
     updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now()))
     user: Optional["User"] = Relationship(back_populates="books")
+    reviews: List["Review"] = Relationship(back_populates="book", sa_relationship_kwargs={"lazy":"selectin"})
+
     def __repr__(self):
-        return f"<Book {self}>"
+        return f"<Book {self.title}>"
 
 
 class BookResponseModel(BaseModel):
@@ -93,3 +96,29 @@ class BookUpdateModel(SQLModel):
     publisher: str
     publication_date: str
     page_count: int
+
+
+
+class Review(SQLModel, table = True):
+    __tablename__ = "reviews"
+
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            primary_key=True,
+            nullable=False,
+            default= uuid.uuid4,
+            unique=True,
+            index=True
+        )
+    )
+    review_text: str
+    rating:int = Field(lt=5) 
+    user_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="users.uid", index=True, ondelete="CASCADE")
+    book_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="books.uid", index=True, ondelete="CASCADE")
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now()))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now()))
+    user: Optional[User] = Relationship(back_populates="reviews")
+    book: Optional[Book] = Relationship(back_populates="reviews")
+    def __repr__(self):
+        return f"<Review for book {self.book_uid} by user {self.user_uid}>"
