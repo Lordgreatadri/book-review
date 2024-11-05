c257183b7e50355fb8c014fe066433fd59c8ec6b
Lordgreat <rexmerlo@gmail.com>
2024-11-05 20:11:26 +0000

    tag feature added

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from src.books.routes import router as book_router
 from src.auth.routes import auth_router
 from src.reviews.routes import review_router
+from src.tags.routes import tags_router
 from contextlib import asynccontextmanager 
 from src.db.main import init_db
 
@@ -28,4 +29,6 @@ app = FastAPI(
 
 app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["Authentication"])
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=["Books"])
-app.include_router(review_router, prefix=f"/api/{version}/reviews", tags=["Revies"])
+app.include_router(review_router, prefix=f"/api/{version}/reviews", tags=["Reviews"])
+app.include_router(tags_router, prefix=f"/api/{version}/tags", tags=["Tags"])
+
